4bd295b8c8790aa92488fc48fb081b5c93b30755
eduardo fulgencio <eduardofulgencio@macbook-pro-de-eduardo.local>
2016-02-03 23:52:56 +0100

    Primera versión.

--- MapDragPinEFC/ViewController.swift
+++ MapDragPinEFC/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var mapView: MKMapView!{
+        didSet {
+            mapView.mapType = .Standard
+            mapView.delegate = self
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func longPressAddPin(sender: AnyObject) {
+        if sender.state == UIGestureRecognizerState.Began {
+            let coordinate = mapView.convertPoint(sender.locationInView(mapView), toCoordinateFromView: mapView)
+            let waypoint = EditableWaypoint(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            waypoint.name = "Añadido pin"
+            mapView.addAnnotation(waypoint)
+        }
+        
+    }
+    
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var view = mapView.dequeueReusableAnnotationViewWithIdentifier(Constants.AnnotationViewReuseIdentifier)
+        
+        if view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: Constants.AnnotationViewReuseIdentifier)
+            view!.canShowCallout = false
+        } else {
+            view!.annotation = annotation
+        }
+        
+        view!.draggable = annotation is EditableWaypoint
+        
+        view!.leftCalloutAccessoryView = nil
+        view!.rightCalloutAccessoryView = nil
+        
+        if let waypoint = annotation as? GPX.Waypoint {
+            if waypoint.thumbnailURL != nil {
+                view!.leftCalloutAccessoryView = UIImageView(frame: Constants.LeftCalloutFrame)
+            }
+            if waypoint.imageURL != nil {
+                view!.rightCalloutAccessoryView = UIButton.init(type:UIButtonType.DetailDisclosure) as UIButton
+            }
+        }
+        
+        return view
     }
 
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let LeftCalloutFrame = CGRect(x: 0, y: 0, width: 59, height: 59)
+        static let AnnotationViewReuseIdentifier = "waypoint"
+    }
 
+    
 }
 
